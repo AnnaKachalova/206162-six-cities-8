53003b2c11e4de099dc09c6f716428a5e22435f4
Anna Kachalova <kachalova@uchi.ru>
2021-11-12 16:03:47 +0300

    add favorites

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,9 +4,9 @@ import { AppRoute } from '../../const';
 import Main from '../main/main';
 import Login from '../login/login';
 import Room from '../room/room';
-//import Favorites from '../favorites/favorites';
+import Favorites from '../favorites/favorites';
 import NotFound from '../not-found/not-found';
-//import PrivateRoute from '../private-route/private-route';
+import PrivateRoute from '../private-route/private-route';
 
 import LoadingScreen from '../loading-screen/loading-screen';
 import { State } from '../../types/state';
@@ -36,6 +36,12 @@ function App(props: PropsFromRedux): JSX.Element {
         <Route exact path={AppRoute.Room}>
           <Room />
         </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <Favorites />}
+        >
+        </PrivateRoute>
         <Route>
           <NotFound />
         </Route>
@@ -46,11 +52,3 @@ function App(props: PropsFromRedux): JSX.Element {
 
 export { App };
 export default connector(App);
-/*
-        <PrivateRoute
-          exact
-          path={AppRoute.Favorites}
-          render={() => <Favorites />}
-          authorizationStatus={AuthorizationStatus.NoAuth}
-        >
-        </PrivateRoute> */
